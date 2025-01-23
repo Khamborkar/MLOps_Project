d0b1e7d3ea5fc01fb439f00a7e03e8940637eead
Khamborkar <2023aa05169@wilp.bits-pilani.ac.in>
2025-01-24 08:17:09 +0900

    model.py
    model.py file is created.

---
+++ src/model.py
@@ -0,0 +1,10 @@
+class SimpleLinearRegression:
+    def __init__(self):
+        self.coef_ = None
+        self.intercept_ = None
+
+    def fit(self, X, y):
+        # ... (Implementation of linear regression training) ...
+
+    def predict(self, X):
+        # ... (Implementation of prediction) ... 
