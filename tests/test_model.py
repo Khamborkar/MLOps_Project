eb4b65cc8a50f0af5a7b4beb933f8c3b5dafb419
Khamborkar <2023aa05169@wilp.bits-pilani.ac.in>
2025-01-24 08:18:29 +0900

    test_model.py
    test_model.py file is created.

---
+++ tests/test_model.py
@@ -0,0 +1,7 @@
+import unittest
+from src.model import SimpleLinearRegression
+
+class TestSimpleLinearRegression(unittest.TestCase):
+    def test_prediction(self):
+        model = SimpleLinearRegression()
+        # ... (Create sample data and test predictions) ...
