e24c9b08e5403dc7b4983d024a46a5b24732ee04
Khamborkar <2023aa05169@wilp.bits-pilani.ac.in>
2025-01-25 17:57:21 +0900

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from src.model import model, clean_text, preprocess_text, remove_stop_words
+from model import model, clean_text, preprocess_text, remove_stop_words
 
 
 class TestModel(unittest.TestCase):
