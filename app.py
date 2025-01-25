745c65cb9d4becc5c18e33f8ab65acce40f80b38
Khamborkar <2023aa05169@wilp.bits-pilani.ac.in>
2025-01-26 02:52:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,11 @@ app = Flask(__name__)
 
 @app.route('/api/sentiment', methods=['POST'])
 # Load the model and tokenizer once at the start
-try:
-    model, tokenizer = load_model_and_tokenizer()
-except Exception as e:
-    print(f"Error loading model and tokenizer: {e}")
+if 1:
+    try:
+        model, tokenizer = load_model_and_tokenizer()
+    except Exception as e:
+        print(f"Error loading model and tokenizer: {e}")
 
 # Define a route to predict sentiment from text
 @app.route('/predict', methods=['POST'])
