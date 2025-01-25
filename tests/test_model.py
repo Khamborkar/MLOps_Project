7a79cc16f37b7f410cf87bbdfb126d18d79e2455
Khamborkar <2023aa05169@wilp.bits-pilani.ac.in>
2025-01-25 18:10:42 +0900

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,7 +4,6 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.callbacks import EarlyStopping
 from model import build_model, clean_text, preprocess_text, remove_stop_words
 
 
